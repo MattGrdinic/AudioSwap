107bfce838ac8e3f6516dd58076aa07f347b7a25
Matthew Grdinic <info@rackforms.com>
2024-02-10 10:09:13 -0700

    Styling, etc

---
+++ scripts.js
@@ -0,0 +1,115 @@
+var audioFiles = [
+  "Denon-PMA2500NE.mp3",
+  "NAD-C388.mp3",
+  "NAIM-NAIT-XS3.mp3",
+  "Yamaha-A-S1200.mp3",
+  "Primare-i25.mp3",
+];
+var sources = [];
+var gainNodes = [];
+var currentTrackIndex = -1;
+var intervalId; // Variable to store the interval ID
+
+function loadTracks() {
+  stopAudio(); // Stop the audio if it is already playing
+
+  var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  var buffers = [];
+  var loadedCount = 0;
+
+  function loadAudio(url, index) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function () {
+      audioContext.decodeAudioData(request.response, function (buffer) {
+        buffers[index] = buffer;
+        loadedCount++;
+
+        if (loadedCount === audioFiles.length) {
+          playTracks();
+        }
+      });
+    };
+
+    request.send();
+  }
+
+  function playTracks() {
+    for (var i = 0; i < buffers.length; i++) {
+      var source = audioContext.createBufferSource();
+      source.buffer = buffers[i];
+      var gainNode = audioContext.createGain();
+      gainNode.gain.value = 0; // Set the initial gain value to 0 (muted)
+      source.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+      sources.push(source);
+      gainNodes.push(gainNode);
+      source.onended = function () {
+        stopAudio();
+      };
+    }
+
+    sources.forEach(function (source) {
+      source.start();
+    });
+
+    // start playing immediately
+    currentTrackIndex = getRandomTrackIndex(
+      Math.floor(Math.random() * audioFiles.length)
+    );
+    updateTrackMuteState(-1, currentTrackIndex);
+    updateTrackName(currentTrackIndex); // Update the track name
+
+    intervalId = setInterval(function () {
+      var previousTrackIndex = currentTrackIndex;
+      currentTrackIndex = getRandomTrackIndex(previousTrackIndex);
+      updateTrackMuteState(previousTrackIndex, currentTrackIndex);
+      updateTrackName(currentTrackIndex); // Update the track name
+    }, 3000);
+  }
+
+  function getRandomTrackIndex(previousIndex) {
+    var index;
+    do {
+      index = Math.floor(Math.random() * audioFiles.length);
+    } while (index === previousIndex);
+    return index;
+  }
+
+  function updateTrackMuteState(previousIndex, currentIndex) {
+    if (previousIndex !== -1) {
+      gainNodes[previousIndex].gain.value = 0; // Mute previous track
+    }
+    gainNodes[currentIndex].gain.value = 1; // Unmute current track
+  }
+
+  function updateTrackName(currentIndex) {
+    var trackNameElement = document.getElementById("trackName");
+    trackNameElement.textContent = audioFiles[currentIndex].replace(".mp3", ""); // Set the track name
+  }
+
+  audioFiles.forEach(function (file, index) {
+    loadAudio(
+      "mp3/" + document.getElementById("track").value + "/" + file,
+      index
+    );
+  });
+}
+
+function stopAudio() {
+  sources.forEach(function (source) {
+    source.stop();
+  });
+  sources = [];
+  gainNodes = [];
+  currentTrackIndex = -1;
+  document.getElementById("trackName").textContent = ""; // Clear the track name
+  clearInterval(intervalId); // Clear the interval
+}
+
+function resetAudio() {
+  stopAudio();
+  loadTracks();
+}
